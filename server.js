7516614dea260dae401791b0f90f7d979b6e5308
Anshu S Panda <anshuspanda@gmail.com>
2018-02-28 00:52:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles = {
-    "article-one": {
+    "articleOne": {
         title: "Article One",
         name: "Article One | Anshu S Panda",
         date: "01/02/2018",
@@ -14,7 +14,7 @@ var articles = {
         <p>This is the place where the article one is shown.This is the place where the article one is shown.This is the place where the article one is shown.This is the place where the article one is shown.This is the place where the article one is shown.This is the place where the article one is shown.</p>
         `
     },
-    "article-two": {
+    "articleTwo": {
         title: "Article Two",
         name: "Article Two | Anshu S Panda",
         date: "07/02/2018",
@@ -22,7 +22,7 @@ var articles = {
         <p>This is the place where the article two is shown.This is the place where the article two is shown.This is the place where the article two is shown.This is the place where the article two is shown.This is the place where the article two is shown.This is the place where the article two is shown.</p>
         `
     },
-    "article-three": {
+    "articleThree": {
         title: "Article Three",
         name: "Article Three | Anshu S Panda",
         date: "27/02/2018",
