42ab033e45bd2355d65ccba29c4ae2c05dc8467e
Anshu S Panda <anshuspanda@gmail.com>
2018-03-02 01:45:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,6 +80,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+var pool=new Pool(config);
+app.get('/test-db',function(req,res) {
+    pool.query("SELECT * FROM test", function(err,result) {
+       if(err) {
+           res.status(500).send(err.toString());
+       } 
+       else {
+           res.send(JSON.stringify(result));
+       }
+    });
+});
+
 app.get('/articles/:articleName',function(req, res){
     
     pool.query("SELECT * FROM article WHERE title=$1", [req.params.articleName], function(err, result) {
@@ -99,17 +112,6 @@ app.get('/articles/:articleName',function(req, res){
 
 });
 
-var pool=new Pool(config);
-app.get('/test-db',function(req,res) {
-    pool.query("SELECT * FROM test", function(err,result) {
-       if(err) {
-           res.status(500).send(err.toString());
-       } 
-       else {
-           res.send(JSON.stringify(result));
-       }
-    });
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
